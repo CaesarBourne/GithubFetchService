76c5a2eaeaa7ba26a6c792761389d0a8654a6838
“CaesarBourne” <caesaradek@gmail.com>
2024-08-08 23:47:37 +0100

    added github service and constants

---
+++ src/services/GithubService.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+import { GITHUB_BASE_URL } from "../lib/constant";
+
+export const getRepositoryInfo = async (owner: string, repository: string) => {
+  const repoResponse = await axios.get(
+    `${GITHUB_BASE_URL}/${owner}/${repository}`
+  );
+  return repoResponse.data;
+};
+
+export const getCommitsData = async (
+  owner: string,
+  repository: string,
+  since?: string
+) => {
+  const commitResponse = await axios.get(
+    `${GITHUB_BASE_URL}/${owner}/${repository}/commiots`,
+    { params: { since } }
+  );
+  return commitResponse.data;
+};
