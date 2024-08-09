04a4d14849cc118741b463246a6c5705097ab571
“CaesarBourne” <caesaradek@gmail.com>
2024-08-09 08:58:39 +0100

    made changes to reflect repository entity name

--- src/entity/RepositoryEntity.ts
+++ src/entity/RepositoryEntity.ts
@@ -1,7 +1,7 @@
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
-export class Repository {
+export class RepositoryEntity {
   @PrimaryGeneratedColumn()
   id!: number;
 
