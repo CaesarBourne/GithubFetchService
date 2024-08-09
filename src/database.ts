3955be953bc17c4adbcddcb5993a325934e2ff10
“CaesarBourne” <caesaradek@gmail.com>
2024-08-10 00:58:17 +0100

    modified code to initiate and seed database

--- src/database.ts
+++ src/database.ts
@@ -1,4 +1,4 @@
-import "reflect-metadata";
+// import "reflect-metadata";
 import { DataSource } from "typeorm";
 import { RepositoryEntity } from "./entity/RepositoryEntity";
 import { CommitEntity } from "./entity/CommitEntity";
