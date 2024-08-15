78abf1a7e98906fa1f5acbaeedabe471bff47904
“CaesarBourne” <caesaradek@gmail.com>
2024-08-15 11:32:34 +0100

    test script updated

--- src/__tests__/GithubFetchService.test.ts
+++ src/__tests__/GithubFetchService.test.ts
@@ -2,15 +2,15 @@ import { initiateMonitoring } from "../services/MonitorRepositoryService"; // Ad
 import { AppDataSource } from "../database"; // Adjust the path to your database config
 
 describe("initiateMonitoring", () => {
-  //   beforeAll(async () => {
-  //     // Initialize the database before tests run
-  //     await AppDataSource.initialize();
-  //   });
+  beforeAll(async () => {
+    // Initialize the database before tests run
+    await AppDataSource.initialize();
+  });
 
-  //   afterAll(async () => {
-  //     // Close the database connection after all tests have run
-  //     await AppDataSource.destroy();
-  //   });
+  afterAll(async () => {
+    // Close the database connection after all tests have run
+    await AppDataSource.destroy();
+  });
 
   it("should start monitoring for the given repository", async () => {
     const owner = "CaesarBourne";
@@ -21,10 +21,10 @@ describe("initiateMonitoring", () => {
 
     // Expect the result to be defined and successful
     expect(result).toBeDefined();
-    // expect(result.success).toBe(true);
-    // expect(result.message).toBe(
-    //   `Monitoring started for ${owner}/${repository}.`
-    // );
+    expect(result.success).toBe(true);
+    expect(result.message).toBe(
+      `Monitoring started for ${owner}/${repository}.`
+    );
   });
 
   it("should not start monitoring if a job is already running", async () => {
@@ -40,9 +40,9 @@ describe("initiateMonitoring", () => {
 
     // Expect the second attempt to fail because the job is already running
     expect(result).toBeDefined();
-    // expect(result.success).toBe(false);
-    // expect(result.message).toBe(
-    //   `A monitoring job is already running for ${owner}/${repository}.`
-    // );
+    expect(result.success).toBe(false);
+    expect(result.message).toBe(
+      `A monitoring job is already running for ${owner}/${repository}.`
+    );
   });
 });
